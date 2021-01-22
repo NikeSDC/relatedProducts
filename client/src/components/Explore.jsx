0e37a839a64c10756854107b613f12fe46395621
Vini Bjazevich <vinibjazevich97@gmail.com>
2021-01-22 12:41:16 -0800

    MVP for Explore done

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Explore = (props) => {
   return (
-    <div>
-      Hello from explore
+    <div className="explore">
+      <p className="exploreText">Explore the LeBron 18</p>
+      <img className="exploreImage" id="firstExploreImage" src="https://nikerelatedproductsimages.s3-us-west-1.amazonaws.com/Nike+Images/SittingOnRailingTop.jpg"></img>
+      <h3 className="exploreHeader">Power On the Court</h3>
+      <p className="exploreText">Max Air provides impact cushioning under the heel. A molded TPU heel counter helps stabilize the foot while you generate force on the court.</p>
+      <img className="exploreImage" id="secondExploreImage" src="https://nikerelatedproductsimages.s3-us-west-1.amazonaws.com/Nike+Images/walkingUpStairs.jpg"></img>
+      <h3 className="exploreHeader">Generate Speed</h3>
+      <p className="exploreText">A full-length Zoom Air unit is nestled within the Max Air under the heel and extends into the forefoot. The high-volume unit helps provide energy return that's fine-tuned for speed.</p>
+      <img className="exploreImage" id="thirdExploreImage" src="https://nikerelatedproductsimages.s3-us-west-1.amazonaws.com/Nike+Images/sittingOnRail.jpg"></img>
+      <h3 className="exploreHeader">Lace Up, Lock In</h3>
+      <p className="exploreText">KnitPosite 2.0 is lightweight and breathable, with knit-in stretch for a secure fit. Laces feed through tunnels engineered into the knit, helping anchor you down without extra weight.</p>
+      <div id="mightIntrest">YOU MIGHT ALSO LIKE</div>
     </div>
   )
 }

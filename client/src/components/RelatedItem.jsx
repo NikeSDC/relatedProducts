04c35809e624610265b1dd2dfec9322cf7d50450
Vini Bjazevich <vinibjazevich97@gmail.com>
2021-01-23 09:36:08 -0800

    Dynamically render sale price. Fixed spacing between info on carousel. Info section does not get cut off on scale down.

--- client/src/components/RelatedItem.jsx
+++ client/src/components/RelatedItem.jsx
@@ -1,18 +1,38 @@
+/* eslint-disable */
 import React from 'react';
 
 const RelatedItem = (props) => {
-  return (
-    <div className="relatedItem" id={`slide-${props.count}`}>
-      <div className="image">
-        <img src={props.relatedItem.media.smallImageUrl} ></img>
+  // On sale
+  if (props.relatedItem.currentPrice) {
+    return (
+      <div className="relatedItem" id={`slide-${props.count}`}>
+        <div className="image">
+          <img src={props.relatedItem.media.smallImageUrl} ></img>
+        </div>
+        <div className="info">
+          <div className="shoeName">{props.relatedItem.title}</div>
+          <div className="category">Basketball Shoe</div>
+          <div className="price">{`$${props.relatedItem.currentPrice}`}</div>
+          <span className="originalPrice">{`$${props.relatedItem.retailPrice}`}</span>
+        </div>
       </div>
-      <div className="info">
-        <div className="shoeName">{props.relatedItem.title}</div>
-        <div className="category">Basketball Shoe</div>
-        <div className="price">{`$${props.relatedItem.retailPrice}`}</div>
+    )
+    // Not on sale  <span className="originPrice">{`${props.relatedItem.retailPrice}`}</span>
+  } else {
+    return (
+      <div className="relatedItem" id={`slide-${props.count}`}>
+        <div className="image">
+          <img src={props.relatedItem.media.smallImageUrl} ></img>
+        </div>
+        <div className="info">
+          <div className="shoeName">{props.relatedItem.title}</div>
+          <div className="category">Basketball Shoe</div>
+          <div className="price">{`$${props.relatedItem.retailPrice}`}</div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 export default RelatedItem;

ba6be9a0db7ef58ca278818a5f1d4d0c0b1c3a74
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-02-04 20:36:23 -0800

    Connected front end with backend w/ refactor to postgreSQL

--- database/index.js
+++ database/index.js
@@ -1,20 +1,31 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/relatedItems', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => { console.log('Connected to mongoose'); })
-  .catch((err) => { console.log(err); });
+// mongoose.connect('mongodb://localhost/relatedItems', { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => { console.log('Connected to mongoose'); })
+//   .catch((err) => { console.log(err); });
 
-const shoeDescriptionSchema = new mongoose.Schema({
-  brand: String,
-  type: { type: String, default: 'Basketball Shoe' },
-  retailPrice: { type: Number, default: 200 },
-  currentPrice: { type: Number, default: null },
-  title: String,
-  media: {
-    smallImageUrl: String,
-  },
-});
+// const shoeDescriptionSchema = new mongoose.Schema({
+//   brand: String,
+//   type: { type: String, default: 'Basketball Shoe' },
+//   retailPrice: { type: Number, default: 200 },
+//   currentPrice: { type: Number, default: null },
+//   title: String,
+//   media: {
+//     smallImageUrl: String,      <---------- Inside of RelatedItems this is being referrenced
+//   },
+// });
 
-const shoeDescription = mongoose.model('shoeDescription', shoeDescriptionSchema);
+// const shoeDescription = mongoose.model('shoeDescription', shoeDescriptionSchema);
 
-module.exports = shoeDescription;
+// module.exports = shoeDescription;
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'gregorie6112',
+  password: '',
+  host: 'localhost',
+  database: 'relatedItems',
+  port: 5432,
+})
+
+module.exports = pool;
